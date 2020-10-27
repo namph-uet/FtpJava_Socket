362f4fb949f76e926b83f6a2a37df5984155e8bb
Phạm Hoàng Nam <38358493+namph-uet@users.noreply.github.com>
2020-10-27 17:56:42 +0700

    update

--- Server/Server.java
+++ Server/Server.java
@@ -7,14 +7,14 @@ public class Server {
     public final static int FILE_READER_BUFF = 4096;
     public final static int MESSAGE_BUFF = 4096;
 
-    public static void main (String [] args ) {
+    public static void main(String[] args) {
         // define variable
         FileInputStream fis = null;
         BufferedInputStream bis = null;
         byte[] messageBuff = new byte[MESSAGE_BUFF];
         OutputStream os = null;
         InputStream is;
-        ServerSocket listener  = null;
+        ServerSocket listener = null;
         Socket sock = null;
         File requestFile = null;
         String message = null;
@@ -33,7 +33,6 @@ public class Server {
         String TYPE_START = "Type start to start download";
 
         try {
-            System.out.println("Waiting for a client...");
             try {
                 listener = new ServerSocket(SOCKET_PORT);
             } catch (IOException e) {
@@ -42,12 +41,13 @@ public class Server {
             }
 
             while (true) {
+                System.out.println("Waiting for a client...");
                 sock = listener.accept();
                 System.out.println("Accepted connection : " + sock);
                 os = sock.getOutputStream();
                 is = sock.getInputStream();
 
-                while(true) {
+                while (true) {
                     wrongSyntax = true;
                     // read message from client
                     messageBuff = new byte[MESSAGE_BUFF];
@@ -57,22 +57,20 @@ public class Server {
 
                     message = message.replaceAll("\r\n", "").replaceAll("\u0000.*", "");
 
-                    if(!servReady && !message.equalsIgnoreCase("HELLO SERVER")) {
+                    if (!servReady && !message.equalsIgnoreCase("HELLO SERVER")) {
                         os.write(NOT_READY.getBytes());
                         wrongSyntax = false;
                     }
 
-                    if(message.equalsIgnoreCase("HELLO SERVER")) {
+                    if (message.equalsIgnoreCase("HELLO SERVER")) {
                         servReady = true;
                         wrongSyntax = false;
                         os.write(HELLO_CLIENT.getBytes());
-                    }
-                    else if(message.equalsIgnoreCase("DOWNLOAD FILE")) {
+                    } else if (message.equalsIgnoreCase("DOWNLOAD FILE")) {
                         os.write(DOWNLOAD_FILE_OK.getBytes());
                         typingFileName = true;
                         wrongSyntax = false;
-                    }
-                    else if(typingFileName) {
+                    } else if (typingFileName) {
                         try {
                             filename = message;
                             requestFile = new File(filename);
@@ -83,7 +81,8 @@ public class Server {
                             System.out.println("Size: " + requestFile.length());
                             typingFileName = false;
                             haveFile = true;
-                            os.write(TYPE_START.getBytes());
+                            String response = "file size:" + String.valueOf(requestFile.length()) + "," + TYPE_START;
+                            os.write(response.getBytes());
                             wrongSyntax = false;
 
                         } catch (FileNotFoundException ex) {
@@ -93,15 +92,15 @@ public class Server {
                         }
                     }
 
-                    if(message.equalsIgnoreCase("START") && haveFile) {
-                        byte [] buffer = new byte[FILE_READER_BUFF];
+                    if (message.equalsIgnoreCase("START") && haveFile) {
+                        byte[] buffer = new byte[FILE_READER_BUFF];
                         wrongSyntax = false;
                         int count = 0;
 
                         requestFile = new File(filename);
                         fis = new FileInputStream(requestFile);
 
-                        while(fis.read(buffer) > 0) {
+                        while (fis.read(buffer) > 0) {
                             os.write(buffer);
                             System.out.println(new String(buffer));
                             os.flush();
@@ -111,7 +110,9 @@ public class Server {
                         wrongSyntax = false;
                     }
 
-                    if(wrongSyntax) {
+                    if (message.equalsIgnoreCase("quit")) break;
+
+                    if (wrongSyntax) {
                         os.write(WRONG_SYNTAX.getBytes());
                         wrongSyntax = false;
                     }
@@ -125,7 +126,7 @@ public class Server {
                 try {
                     if (bis != null) bis.close();
                     if (os != null) os.close();
-                    if (sock!=null) sock.close();
+                    if (sock != null) sock.close();
                     sock.close();
                 } catch (IOException e) {
                     e.printStackTrace();
@@ -134,4 +135,3 @@ public class Server {
         }
     }
 }
-
